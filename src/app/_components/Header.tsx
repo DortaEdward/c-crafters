d69259e09e3d58a81e346fc54cfb11a632aeea6e
dortaedward <dortaedward@gmail.com>
2024-03-18 15:06:41 -0400

    removed isLoading for deployment

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -4,28 +4,23 @@ import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs"
 import Link from "next/link"
 
 export default function Header(){
-  const {isSignedIn, isLoading, user} = useUser()
+  const {isSignedIn, user} = useUser()
 
   return(
     <header className="w-full py-4 px-10 md:px-20 border-b flex justify-between items-center">
       <div>
         <Link href={`${isSignedIn ? "/dashboard" :"/"}`} className="text-2xl font-bold outline-none">C/Crafter</Link>
       </div>
-      {
-        isLoading
-          ? <>Skeleton</>
-          :
-          <div>
-            {
-              isSignedIn
-                ? <div className="flex gap-2">
-                  <Link href="/dashboard" className="font-semibold text-lg">Dashboard</Link>
-                  <SignOutButton className="font-medium text-lg" />
-                </div>
-                : <SignInButton />
-            }
+      <div>
+        {
+          isSignedIn
+            ? <div className="flex gap-2">
+              <Link href="/dashboard" className="font-semibold text-lg">Dashboard</Link>
+              <SignOutButton className="font-medium text-lg" />
+            </div>
+            : <SignInButton />
+        }
       </div>
-      }
     </header>
   )
 }
